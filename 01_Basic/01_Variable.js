1f47ec70457861eb173f2a09df4b673ed7f44afa
Lalit97614 <lalitrajpoot976148@gmail.com>
2025-12-07 18:35:37 +0000

    Variable is defined

---
+++ 01_Basic/01_Variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "lalitrajpoot@123.gmail.com"
+var accountPassword = "12345"
+accountCity= "Jaipur"
+// accountId=2
+console.log(accountId)
+
+
+accountEmail="ramkumar@123"
+accountPassword="212121"
+accountCity="Dibai"
+console.table([accountId,accountEmail,accountPassword,accountCity])
