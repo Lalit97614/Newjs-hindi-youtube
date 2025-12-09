5534034c52475cc9887b7cc59327703a89007b89
Lalit97614 <lalitrajpoot976148@gmail.com>
2025-12-09 19:09:04 +0000

    datacomparision is covered

---
+++ 01_Basic/04_DataTypeComparision.js
@@ -0,0 +1,17 @@
+// console.log(2>4)
+// console.log(2<4)
+// cosole.log(2==4)
+
+// console.log("2">1)   //--->true because it change the string into number
+// console.log("3"<2)  //--->false
+
+console.log(null)
+console.log(null>0)   //-->false 
+console.log(null==0)   //-->false 
+console.log(null>=0)  //-->true
+console.log(null<0)  //-->false
+console.log(null<=0)  //-->true
+
+// === ---> triple equal
+console.log("2"==2)  // false
+console.log(2==2)    // true
