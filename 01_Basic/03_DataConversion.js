a1f200286d7077ed322d0b26ff6d5bb616fe143e
Lalit97614 <lalitrajpoot976148@gmail.com>
2025-12-09 18:42:19 +0000

    data conversion

---
+++ 01_Basic/03_DataConversion.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+// Operation----->
+let value = 3
+let negvalue = - value
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(10-5)
+// console.log(30*2)
+// console.log(15%4)
+// console.log(2**3)
+// console.log(45/4)
+
+let str1 = "Hello"
+let str2 = " Lalit"
+console.log(str1+str2)
+
+
+
+// console.log("2"+1)
+// console.log(typeof("2"+1))
+// console.log(1+"2")
+// console.log(typeof(1+"2"))
+// console.log("1"+2+3)
+// console.log(typeof("1"+2+3))
+// console.log(1+2+3+"3")
+// console.log(typeof(1+2+3+"3"))
+
+
+// console.log(+true)
+// console.log(1-false+2)
+
+
+
+let gameCounter= 100
+gameCounter++
+console.log(gameCounter)
