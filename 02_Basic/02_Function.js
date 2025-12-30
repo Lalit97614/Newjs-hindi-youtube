585cb0fb6aafb5b385d192a451fd2b71cebb4589
Lalit97614 <lalitrajpoot976148@gmail.com>
2025-12-30 14:22:58 +0000

    function 2 done

---
+++ 02_Basic/02_Function.js
@@ -0,0 +1,23 @@
+function calculatecartPrices(val1,val2,...num1){
+    return num1
+}
+console.log(calculatecartPrices(200,300,400,500))
+
+
+
+const user={
+     username :"Lalit kumar",
+     price:200
+}
+function handdleObject(user1){
+    console.log(`username is ${user1.username} and price is ${user1.price}`)
+}
+
+handdleObject(user)
+
+
+const arr =[200,300,400,500]
+function handdleArray(arr){
+    return arr[1]
+}
+console.log(handdleArray(arr))
