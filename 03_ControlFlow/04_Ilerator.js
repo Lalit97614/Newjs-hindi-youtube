6122304f835b71ffa25047afe1aaddfac607ebe8
Lalit97614 <lalitrajpoot976148@gmail.com>
2026-01-02 11:28:57 +0000

    for  loop done

---
+++ 03_ControlFlow/04_Ilerator.js
@@ -0,0 +1,20 @@
+// for loop
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+
+for(let i =0;i<=10;i++){
+    for(let j =1;j<=10;j++){
+        console.log(`${i} * ${j}=${i*j}`)
+    }
+    console.log()
+}
+
+
+
+let arr = ["Lalit","Ram","Amit","Sachin","raghav","Rahul"]
+for(let i = 0;i<arr.length;i++){
+    console.log(arr[i])
+}
