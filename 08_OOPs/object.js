87d9e662edeace106fd6cc482883689ce9488300
Lalit Kumar <lalitrajpoot976148@gmail.com>
2026-05-23 07:34:08 +0000

    again ob

--- 08_OOPs/object.js
+++ 08_OOPs/object.js
@@ -11,6 +11,21 @@ console.log(multiplyBy5.prototype);
 
 
 function createUser(username,score){
-    this.username =username
+    this.username =username;
     this.score = score;
 }
+
+
+createUser.prototype.increment=function(){
+    this.score++;
+}
+ 
+createUser.prototype.printMe=function(){
+    console.log(`score is ${this.score}`);
+}
+
+
+const chai = new createUser("chai",25);
+const tea =  new createUser("tea",250);
+
+chai.printMe()
